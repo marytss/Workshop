8898a7b461d0d28c858c74432aac500352c1309f
Natasha-rare <natasha_ea5@mail.ru>
2020-04-20 18:51:54 +0300

    исправлены ошибки
    ДЗ

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+      Console.WriteLine("Hello World");
+      int a = 4;
+      int b = 3;
+      int c = a + b;
+      Console.WriteLine(c);  }
+}
